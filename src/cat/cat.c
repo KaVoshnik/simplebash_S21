b20bd4705450ca1c65ce1c58947c89a994f66bf2
Kirill <112951242+KaVoshnik@users.noreply.github.com>
2025-04-23 12:35:50 +0700

    Update cat.c

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -1,10 +1,33 @@
-#include <stdio.h>
+#include <stdio.h>  // NyanCat Yooooo
 
-int main() {
+void print_file(char *name) {
+    FILE *f = fopen(name, "rt");
 
+    if (f != NULL) {
+        int c = fgetc(f);
+        while (c != EOF) {
+            putc(c, stdout);
+            c = fgetc(f);
+        }
+        fclose(f);
+    }
+}
+
+int main(int argc, char **argv) {
+    for (int i = 1; i < argc; i++) {
+        print_file(argv[i]);
+    }
+    return 0;
 }
 
 /*
-1. take file
-2. print content
+1. take file - Yes
+2. print content - Yes
+3. Take many files - Yes
+Flags: - No
+-b - No
+-e - No
+-n - No
+-s - No
+-t - No
 */
